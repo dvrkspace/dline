c070a08442cae3220826df541f48a81dbe8bd1d1
dvrkspace <63521031+dvrkspace@users.noreply.github.com>
2020-09-17 19:55:04 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,32 +2,22 @@ import telebot
 
 bot = telebot.TeleBot('943862443:AAGJGrMorC-XAvCuMqBLYMoMJQ1Aa1-HGu0')
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Web Technology')
-keyboard1.row('English')
-keyboard1.row('Discrete Math')
-keyboard1.row('Database')
-keyboard1.row('Software Development')
+keyboard1.row('Привет', 'Пока')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Доброго времени суток! /start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'Web Technology':
-        bot.send_message(message.chat.id, 'Понедельник, 21 Сентябрь 2020, 00:02 \n\n Laboratory work1_for_IT2CCO-2001 ')
-    elif message.text.lower() == 'English':
-        bot.send_message(message.chat.id, 'Понедельник 21 September 2020, 00:02  Hometask. N WB. Unit 6 p. 38 Задание \n\n English Четверг, 24 Сентябрь 2020, 00:02  Glossary ')
-    elif message.text.lower() == 'Discrete Math':
-        bot.send_message(message.chat.id, '22 сентября до 15:00  exercise №3')
-    elif message.text.lower() == 'Database':
-        bot.send_message(message.chat.id, 'Среда 23 сентября 2020, 00:02  Course Work 1, 2')
-    elif message.text.lower() == 'Software Development':
-        bot.send_message(message.chat.id, 'Суббота 27 September 2020, 23:55  lab 2 \n\n Воскресенье, 4 Октябрь 2020, 23:55  lab 3 ')
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+
     print(message)
-
-
-
-    
 
 bot.polling()
